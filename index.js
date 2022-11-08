5ce46260cd297cd67f5f37ba97ddc9c7e8274268
Alex Haiduc <haiduc.alx@gmail.com>
2022-11-08 12:21:08 +0200

    Create, store and display numbers Added event listener for each number button. Created event handler createNumber(), targeted each time a button is clicked. createNumber() displays the value selected on the calculator screen, and returns the firstValue or secondValue. Added listener for '+'button, to be able to return the secondValue, temporary method, to be able to test if createNumber() works for both values.

--- index.js
+++ index.js
@@ -17,3 +17,39 @@ const divide = function(a,b) {
 const operate = function(operation, a, b) {
     return operation(a,b);
 }
+
+const display = document.getElementById('display');
+let displayValue = 0;
+let firstValue = 0;
+let secondValue = 0;
+let operator = '';
+
+const numbers = Array.from(document.getElementsByClassName('numbers'));
+numbers.forEach(number => number.addEventListener('click', createNumber));
+
+function createNumber(number){
+    let value = number.target.innerText;
+
+    if(displayValue === 0) {
+        displayValue = value;
+    } else {
+        displayValue = `${displayValue}${value}`;
+    }
+
+    if(!operator) {
+        console.log(value)
+        firstValue = Number(`${firstValue}${value}`);
+        return [display.textContent = displayValue, firstValue];
+    } else {
+        secondValue = Number(`${secondValue}${value}`);
+        return [display.textContent = displayValue, secondValue];
+    }
+}
+
+// temporary to test if createNumber() captures both first and second values
+const addition = document.getElementById('add');
+addition.addEventListener('click', function(){
+    console.log(addition.value)
+    displayValue = `${displayValue}${addition.textContent}`;
+    return [operator = add, display.textContent = displayValue];
+})
